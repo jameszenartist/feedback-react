2a172e42c98f558f291da65f1ac1b9b54efc5af4
James Hansen <jameszenartist@gmail.com>
2022-09-23 22:54:18 -0700

    added feedback items

--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
 //latest syntax:
 const root = createRoot(document.getElementById("root"));
 
